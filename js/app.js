023295eb1e5fde1a8d990e40a2b8f66ee9368f07
Adamsh6 <adam.s.houten@gmail.com>
2019-06-16 11:47:28 +0100

    finished base javascript

--- js/app.js
+++ js/app.js
@@ -5,6 +5,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
   setDate(form)
 
+  const wins = {}
+
+  const addPlayerForm = document.querySelector('#add-player-form')
+  addPlayerForm.addEventListener('submit', handleAddPlayer)
+
 
   const deleteButton = document.querySelector('#delete')
   deleteButton.addEventListener('click', handleDeleteButton)
@@ -16,6 +21,17 @@ const setDate = function (form) {
     '-' + date.getDate().toString().padStart(2, 0);
   form.date.value = dateString
   form.date.max = dateString
+}
+
+const handleAddPlayer = function (event) {
+  event.preventDefault();
+  //Add player to table and selector
+  const player = this.addPlayer.value
+  const selector = document.querySelector('#victor')
+  selector.insertAdjacentHTML('beforeend', `<option>${player}</option>`)
+  const table = document.querySelector('#win-table')
+  table.insertAdjacentHTML('beforeend',
+  `<tr><td>${player}</td><td id='${player}-wins'>0</td></tr>`)
 }
 
 const handleFormSubmission = function (event) {
@@ -36,6 +52,13 @@ const handleFormSubmission = function (event) {
   victor.textContent = "Victor: " + this.victor.value
   game.textContent = this.game.value + ' : ' + this.gameType.value
   date.textContent = this.date.value
+
+  addWin(this.victor.value)
+}
+
+const addWin = function (victor) {
+  const playerWin = document.querySelector(`#${victor}-wins`)
+  playerWin.textContent = parseInt(playerWin.textContent) + 1
 }
 
 
